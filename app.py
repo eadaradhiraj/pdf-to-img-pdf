dd36e9e693f5bb5d3b6e158b46f881e3977e4c4b
Dhiraj Eadara <eadaradhiraj@gmail.com>
2023-07-12 23:40:32 +0530

    Add templates

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import base64
 from pdf_to_img_pdf import PDF2IMGPDF
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 import json
 
 
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1000 * 1000
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
+
+
+@app.route('/')  
+def main():
+    return render_template("page.html")
 
 
 @app.route("/download-pdf", methods=["POST"])
-# @cross_origin()
 def download_pdf():
     posted_data = json.loads(request.data)
     if not posted_data.get("images", []):
@@ -24,26 +28,26 @@ def download_pdf():
 
 
 @app.route("/upload-pdf", methods=["POST"])
-# @cross_origin()
 def upload_pdf():
     # check if the post request has the file partx
-    if "pdf" not in request.form:
-        resp = jsonify({"message": "No pdf in the request"})
-        resp.status_code = 400
+    if request.method == 'POST':
+        if "pdf" not in request.form:
+            resp = jsonify({"message": "No pdf in the request"})
+            resp.status_code = 400
+            return resp
+        if request.form.get("pdf") == "":
+            resp = jsonify({"message": "No file selected for uploading"})
+            resp.status_code = 400
+            return resp
+        pdf_bytes = request.form.get("pdf")
+        if len(pdf_bytes) > 0:
+            images = PDF2IMGPDF.convert_to_images(base64.b64decode(pdf_bytes.encode()))
+            resp = jsonify({"images": PDF2IMGPDF.decode_images(images)})
+            resp.status_code = 201
+        else:
+            resp = jsonify({"message": "Allowed file types are pdf"})
+            resp.status_code = 400
         return resp
-    if request.form.get("pdf") == "":
-        resp = jsonify({"message": "No file selected for uploading"})
-        resp.status_code = 400
-        return resp
-    pdf_bytes = request.form.get("pdf")
-    if len(pdf_bytes) > 0:
-        images = PDF2IMGPDF.convert_to_images(base64.b64decode(pdf_bytes.encode()))
-        resp = jsonify({"images": PDF2IMGPDF.decode_images(images)})
-        resp.status_code = 201
-    else:
-        resp = jsonify({"message": "Allowed file types are pdf"})
-        resp.status_code = 400
-    return resp
 
 
 if __name__ == "__main__":
