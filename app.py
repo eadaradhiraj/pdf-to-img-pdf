a00b1d6751f9831e5a51d2c4f71b0faf76cbdb37
Dhiraj Eadara <eadaradhiraj@gmail.com>
2023-07-12 14:09:25 +0530

    add vercel and black

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ import json
 
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1000 * 1000
 CORS(app)
+
 
 @app.route("/download-pdf", methods=["POST"])
 def download_pdf():
@@ -16,9 +17,10 @@ def download_pdf():
         resp = jsonify({"message": "No images in request"})
         resp.status_code = 400
         return resp
-    resp = jsonify({"pdf":PDF2IMGPDF.img_to_pdf(posted_data.get("images"))})
+    resp = jsonify({"pdf": PDF2IMGPDF.img_to_pdf(posted_data.get("images"))})
     resp.status_code = 200
     return resp
+
 
 @app.route("/upload-pdf", methods=["POST"])
 def upload_pdf():
@@ -33,15 +35,11 @@ def upload_pdf():
         return resp
     pdf_bytes = request.form.get("pdf")
     if len(pdf_bytes) > 0:
-        images = PDF2IMGPDF.convert_to_images(
-            base64.b64decode(pdf_bytes.encode())
-        )
+        images = PDF2IMGPDF.convert_to_images(base64.b64decode(pdf_bytes.encode()))
         resp = jsonify({"images": PDF2IMGPDF.decode_images(images)})
         resp.status_code = 201
     else:
-        resp = jsonify(
-            {"message": "Allowed file types are pdf"}
-        )
+        resp = jsonify({"message": "Allowed file types are pdf"})
         resp.status_code = 400
     return resp
 
